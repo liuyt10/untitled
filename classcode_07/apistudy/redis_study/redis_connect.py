6e989298d92c9c1df008a45c9bca6f74e762d487
“liuyt10” <lyt10223@163.com>
2022-08-01 00:07:43 +0800

    Default Changelist

--- classcode_07/apistudy/redis_study/redis_connect.py
+++ classcode_07/apistudy/redis_study/redis_connect.py
@@ -10,12 +10,13 @@ import redis
 
 class RedisUtil:
 
-    def __init__(self,host,pwd,port=6379,decode_responses=False,db=0):
+    def __init__(self, host, pwd, port=6379, decode_responses=False, db=0):
         # 先创建一个连接池
-        self.pool = redis.ConnectionPool(host=host, password=pwd,port=port, decode_responses=decode_responses, db=db)
+        self.pool = redis.ConnectionPool(host=host, password=pwd, port=port, decode_responses=decode_responses, db=db)
         self.r = redis.Redis(connection_pool=self.pool)  # 从连接池里拿到一个连接对象
         # 通过连接对象，对redis进行操作
-    def get(self,key):
+
+    def get(self, key):
         # 判断该key对应的数据类型是什么，然后决定要用哪个数据获取方法
         type = self.r.type(key).decode('utf-8')
         if type == 'string':
@@ -23,25 +24,44 @@ class RedisUtil:
         elif type == 'hash':
             return self.r.hgetall(key)
         elif type == 'list':
-            return self.r.lrange(key,0,-1)
+            return self.r.lrange(key, 0, -1)
         elif type == 'set':
             return self.r.smembers(key)
         elif type == 'zset':
-            return self.r.zrange(key,0,-1)
+            return self.r.zrange(key, 0, -1)
         else:
             raise Exception(f'{key}对应的数据类型不支持')
+
+
 if __name__ == '__main__':
-    redis_util = RedisUtil(host='121.42.15.146',pwd='testfan')
-    res = redis_util.get('{BUY_NOW_ORIGIN_DATA_PREFIX}_59') #59是用户id
+    redis_util = RedisUtil(host='121.42.15.146', pwd='testfan')
+    res = redis_util.get('{CHECKOUT_PARAM_ID_PREFIX}_26224')  # 59是用户id
     # res得到的数据是商城项目向缓存中存储的一个java对象的序列化数据，我们无法直接从中得到想要的数据
     # 所以我们需要先将该数据转换成python对象，然后再处理
-    print(res)
-    res_obejct = javaobj.loads(res)
-    print(res_obejct)
-    print(type(res_obejct))
-    print(res_obejct[0])
-    # 获取该对象下都有哪些属性信息,看到的业务属性需要和开发沟通是什么意思
-    print(dir(res_obejct[0]))
-    # 获取立即购买时存储到redis的num数量
-    print(res_obejct[0].__getattribute__('num'))
-    print(res_obejct[0].__getattribute__('skuId'))
+    # print(res)
+    i = 0
+    a = 0
+    for key, value in res.items():
+        key1 = javaobj.loads(key)
+        try:
+            value1 = javaobj.loads(value)
+            a += 1
+        except:
+            i += 1
+            pass
+        if key1 == 'addressId':
+            print(javaobj.loads(value))
+    print(i)
+    print(a)
+
+
+
+    # res_obejct = javaobj.loads(res)
+    # print(res_obejct)
+    # print(type(res_obejct))
+    # # print(res_obejct[0])
+    # # 获取该对象下都有哪些属性信息,看到的业务属性需要和开发沟通是什么意思
+    # print(dir(res_obejct[0]))
+    # # 获取立即购买时存储到redis的num数量
+    # print(res_obejct[0].__getattribute__('num'))
+    # print(res_obejct[0].__getattribute__('skuId'))
--- homework_20220612_8/test/db_basic.py
+++ homework_20220612_8/test/db_basic.py
@@ -3,12 +3,12 @@
 import pymysql
 
 connect = pymysql.Connect(
+    cuersorclass=pymysql.cursors.DictCursor,  # 这个参数指的是操作的结果是一个字典类型
     host='121.42.15.146',
     port=3306,
     user='root',
     password='Testfan#123',
-    charset='utf8md5',
-    cuersorclass=pymysql.cursors.DictCursor
+    charset='utf8md5'
 )
-
+'{CHECKOUT_PARAM_ID_PREFIX}_26224'
 cursor = connect.cursor()
